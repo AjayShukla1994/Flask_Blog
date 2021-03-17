9075349a5c3af7f4f2ae25a68676275dd1affc49
Ajay Shukla <ajay@finezza.in>
2021-03-18 03:05:16 +0530

    template is done

--- run.py
+++ run.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ajay Shukla',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Amit Shukla',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 10, 2019'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def hellow():
-    return "<h1>Home Page<h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page<h1>"
+    return render_template("about.html", posts=posts, title='About')
 
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0', port = 5000, debug = False)
+    app.run(host ='0.0.0.0', port = 5000, debug = True)
